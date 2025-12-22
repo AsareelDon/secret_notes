f8f60bbe9d23c6e94cfd7f85a2e4b708e3c7c43e
AsareelDon <asareeldonpena@gmail.com>
2025-12-22 16:34:35 +0800

    feat: add generic base use case interface

---
+++ lib/src/core/usecase/generic_usecase.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:secret_notes/src/core/error/failures.dart';
+
+abstract class GenericUseCase<Type, Params> {
+  Future<Either<Failures, Type>> call(Params params);
+}
+
+class NoParams {}

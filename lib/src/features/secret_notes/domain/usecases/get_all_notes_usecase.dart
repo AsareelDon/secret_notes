ed9eda6f11dce94bba87aacfa5f9c737161d11ef
AsareelDon <asareeldonpena@gmail.com>
2026-01-02 09:30:14 +0800

    refactor: repo-interface-impl to return list of the entity

--- lib/src/features/secret_notes/domain/usecases/get_all_notes_usecase.dart
+++ lib/src/features/secret_notes/domain/usecases/get_all_notes_usecase.dart
@@ -4,13 +4,11 @@ import 'package:secret_notes/src/core/usecase/generic_usecase.dart';
 import 'package:secret_notes/src/features/secret_notes/domain/entities/note_entity.dart';
 import 'package:secret_notes/src/features/secret_notes/domain/repository/note_repository.dart';
 
-class GetAllNotesUsecase extends GenericUseCase<NoteEntity?, NoParams>{
+class GetAllNotesUsecase extends GenericUseCase<List<NoteEntity>, NoParams>{
   final NoteRepository noteRepository;
 
   GetAllNotesUsecase({required this.noteRepository});
 
   @override
-  Future<Either<Failures, NoteEntity?>> call(NoParams params) async {
-    return await noteRepository.getAllNotes();
-  }
+  Future<Either<Failures, List<NoteEntity>>> call(NoParams params) => noteRepository.getAllNotes();
 }

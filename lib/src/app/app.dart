caf8bdc4986eb2e0d8490708b29eab1939aa1b47
AsareelDon <asareeldonpena@gmail.com>
2026-01-06 15:01:16 +0800

    feat(theme): add system-based light and dark mode theme

---
+++ lib/src/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:secret_notes/src/core/theme/app_theme.dart';
+import 'package:secret_notes/src/features/secret_notes/presentation/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: HomePage(title: 'Secret Notes'),
+    );
+  }
+}

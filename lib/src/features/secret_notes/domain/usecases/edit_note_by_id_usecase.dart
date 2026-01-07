2833f12d20ba7a369e8d84f8963ebc3f9a25293d
AsareelDon <asareeldonpena@gmail.com>
2026-01-07 16:36:51 +0800

    feat(domain): add usecase for edit-note operation

---
+++ lib/src/features/secret_notes/domain/usecases/edit_note_by_id_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:secret_notes/src/core/error/failures.dart';
+import 'package:secret_notes/src/core/usecase/generic_usecase.dart';
+import 'package:secret_notes/src/features/secret_notes/domain/entities/note_entity.dart';
+import 'package:secret_notes/src/features/secret_notes/domain/repository/note_repository.dart';
+
+class EditNoteByIdUseCase extends GenericUseCase<NoteEntity, EditNoteParams> {
+  final NoteRepository noteRepository;
+
+  EditNoteByIdUseCase({required this.noteRepository});
+
+  @override
+  Future<Either<Failures, NoteEntity>> call(EditNoteParams params) async {
+    return await noteRepository.editNoteById(params.noteEntity);
+  }
+}
+
+class EditNoteParams extends Equatable {
+  final NoteEntity noteEntity;
+
+  const EditNoteParams({required this.noteEntity});
+
+  @override
+  List<Object?> get props => [noteEntity];
+}

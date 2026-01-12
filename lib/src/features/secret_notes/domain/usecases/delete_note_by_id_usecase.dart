7a3cb6fb7ee0e0b7976af4e3dbd4479b244c3b97
AsareelDon <asareeldonpena@gmail.com>
2026-01-12 11:40:09 +0800

    refactor(domain): standardize usecase lifecycle with injectable

--- lib/src/features/secret_notes/domain/usecases/delete_note_by_id_usecase.dart
+++ lib/src/features/secret_notes/domain/usecases/delete_note_by_id_usecase.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
 import 'package:secret_notes/src/core/error/failures.dart';
 import 'package:secret_notes/src/core/usecase/generic_usecase.dart';
 import 'package:secret_notes/src/features/secret_notes/domain/repository/note_repository.dart';
 
+@LazySingleton()
 class DeleteNoteByIdUseCase extends GenericUseCase<int, DeleteNoteParams> {
   final NoteRepository noteRepository;
 
--- lib/src/features/secret_notes/domain/usecases/get_all_notes_usecase.dart
+++ lib/src/features/secret_notes/domain/usecases/get_all_notes_usecase.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:secret_notes/src/core/error/failures.dart';
 import 'package:secret_notes/src/core/usecase/generic_usecase.dart';
 import 'package:secret_notes/src/features/secret_notes/domain/entities/note_entity.dart';
 import 'package:secret_notes/src/features/secret_notes/domain/repository/note_repository.dart';
 
+@LazySingleton()
 class GetAllNotesUsecase extends GenericUseCase<List<NoteEntity>, NoParams>{
   final NoteRepository noteRepository;
 
